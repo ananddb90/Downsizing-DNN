015ba59f139645646e70699adea82e14a91a2c99
anand <dubeyanand.007@gmail.com>
2017-07-04 09:20:32 +0200

    Update train_val.py

--- train_val.py
+++ train_val.py
@@ -1,57 +1,40 @@
+
 import os
 import numpy as np
 
-root = "/imagenet/2011/" 
-val = "/imagenet/2011/bboxes/val/"
+root = '/imagenet/2011/'
+img_label = {}
+val = []
 
-val_label = {}
-gt_label = {}
-
-def create_gt():
-  count = 0
-  with open('/imagenet/2011/train/labels.txt') as f:
-    line = f.readlines()
-  
-  for i in range(len(line)):
-    gt_label[line[i][0:-2]] = count
-    count = count + 1
-    
-  f1 = open(os.path.join(root, "gtlabel.txt"), 'wb')
-  for i in gt_label:
-    f1.write(i + 't' + gt_label[i] + '\n')
-  f1.close()
-  
-def create_train():
+for (par_curpath, par_dirnam, par_filnam) in os.walk(os.path.join(root, 'train')):
+       for i in range(len(par_dirnam)):
+               for (child_curpath, child_dirnam, child_filnam) in os.walk(root + 'train/' + par_dirnam[i]):
+                       for j in range(len(child_filnam)):
+                               img_label[par_dirnam[i] + '/' + child_filnam[j]] = i
 
 
 
-def create_val():
+f = open(os.path.join(root, 'train.txt'),'wb')
+for i in img_label:
+       f.write(i + '\t' + str(img_label[i]) + '\n')
+
+f.close()
 
 
 
+#with open('/rfcn/py-R-FCN/caffe/data/ilsvrc12/val.txt') as file:
+#       filedata = file.read()
+
+f = open('/rfcn/py-R-FCN/caffe/data/ilsvrc12/val.txt').xreadlines()
+
+for l in f:
+        new = l.replace('ILSVRC2012_', 'ILSVRC2011_')
+        val.append(new)
+
+f2 = open(os.path.join(root, 'val.txt'), 'wb')
+for i in val:
+        f2.write(i)
+f2.close()
+f.close()
 
 
-create_gt()
-create_train()
-create_val()
-
-
-
-
-
-
-for (curr_dir, folder, files) in os.walk(val):
-  for i in range(len(files)):
-    filename = curr_dir + files[i]
-    f2 = open(label,'r')
-    for c in ( raw.strip().split() for raw in f2 ):
-        gt_label.append() = c
-        
-    if filename.endwith('.xml') or filename.endwith('.XML'):
-        f1 = open(filename,'r').read()
-        
-        
-        start = f1.find('<name>')
-        end = f1.find('</name>')
-        
-        img_label
